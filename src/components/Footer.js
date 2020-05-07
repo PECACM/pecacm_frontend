d452fe71d8ed46c59dee01b62dfd82e9f4719c8a
TheNsBhasin <nsbhasincool@gmail.com>
2020-05-07 20:31:24 +0530

    Single line footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,7 +11,7 @@ function Footer(props) {
 
       <div className="link">
         <a
-          href="https://github.com/PECACM"
+          href="https://pec.ac.in/pecacm"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -19,70 +19,66 @@ function Footer(props) {
         </a>
       </div>
 
-      <a
-        href="https://github.com/PECACM/pecacm_frontend"
-        className="button github"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <Icon.GitHub />
-        <span>Open Sourced on GitHub</span>
-      </a>
+      <div>
+        <a
+          href="https://github.com/PECACM/pecacm_frontend"
+          className="github"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Icon.GitHub />
+        </a>
 
-      <a
-        href="https://www.facebook.com/pecacm/"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="button facebook"
-        style={{ justifyContent: "center" }}
-      >
-        <Icon.Facebook />
-        <span>View updates on Facebook</span>
-      </a>
+        <a
+          href="https://www.facebook.com/pecacm/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="facebook"
+          style={{ justifyContent: "center" }}
+        >
+          <Icon.Facebook />
+        </a>
 
-      <a
-        href="https://www.instagram.com/pecacm/"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="button instagram"
-        style={{ justifyContent: "center" }}
-      >
-        <Icon.Instagram />
-        <span>View updates on Instagram</span>
-      </a>
+        <a
+          href="https://www.instagram.com/pecacm/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="instagram"
+          style={{ justifyContent: "center" }}
+        >
+          <Icon.Instagram />
+        </a>
 
-      <a
-        href="https://twitter.com/ACM_CSS_PEC"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="button twitter"
-        style={{ justifyContent: "center" }}
-      >
-        <Icon.Twitter />
-        <span>View updates on Twitter</span>
-      </a>
+        <a
+          href="https://twitter.com/ACM_CSS_PEC"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="twitter"
+          style={{ justifyContent: "center" }}
+        >
+          <Icon.Twitter />
+        </a>
 
-      <a
-        href="https://www.linkedin.com/in/pecacm/"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="button linkedin"
-        style={{ justifyContent: "center" }}
-      >
-        <Icon.Linkedin />
-        <span>View updates on LinkedIn</span>
-      </a>
+        <a
+          href="https://www.linkedin.com/in/pecacm/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="linkedin"
+          style={{ justifyContent: "center" }}
+        >
+          <Icon.Linkedin />
+        </a>
 
-      <a
-        href="mailto:acmcss@pec.edu.in?Subject=Contact%20PEC%20ACM%20Student%20Chapter"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="button mail"
-        style={{ justifyContent: "center" }}
-      >
-        <Icon.Mail />
-        <span>Contact us</span>
-      </a>
+        <a
+          href="mailto:acmcss@pec.edu.in?Subject=Contact%20PEC%20ACM%20Student%20Chapter"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="mail"
+          style={{ justifyContent: "center" }}
+        >
+          <Icon.Mail />
+        </a>
+      </div>
     </footer>
   );
 }
