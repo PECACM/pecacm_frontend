d02dda0c508824e77e5cc483a8b60d1daadf9162
Utkarsh Goel <ugoel911@gmail.com>
2020-05-09 00:39:40 +0530

    Expansion Panel Added

--- src/components/AccordianComponent.js
+++ src/components/AccordianComponent.js
@@ -5,7 +5,6 @@ import {
   ExpansionPanelSummary,
 } from "@material-ui/core";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-import "../assets/css/accordian.css";
 
 function Accordian() {
   const [expanded, setExpanded] = React.useState(false);
@@ -13,45 +12,34 @@ function Accordian() {
     setExpanded(isExpanded ? panel : false);
   };
   return (
-    <div className="accordian">
-      <div className="container">
-        <div className="row">
-          <div className="col-9">
-            <ExpansionPanel
-              className="panel"
-              expanded={expanded === "panel1"}
-              onChange={handleChange("panel1")}
-            >
-              <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-                2019-20
-              </ExpansionPanelSummary>
-              <ExpansionPanelDetails>
-                <a href="/" className="link">
-                  Report for year 2019-20
-                </a>
-              </ExpansionPanelDetails>
-            </ExpansionPanel>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-9">
-            <ExpansionPanel
-              className="panel"
-              expanded={expanded === "panel2"}
-              onChange={handleChange("panel2")}
-            >
-              <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-                2018-19
-              </ExpansionPanelSummary>
-              <ExpansionPanelDetails>
-                <a href="/" className="link">
-                  Report for year 2018-19
-                </a>
-              </ExpansionPanelDetails>
-            </ExpansionPanel>
-          </div>
-        </div>
-      </div>
+    <div>
+      <ExpansionPanel
+        className="panel"
+        expanded={expanded === "panel1"}
+        onChange={handleChange("panel1")}
+      >
+        <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+          2019-20
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          <a href="/" className="link">
+            Report for year 2019-20
+          </a>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+      <ExpansionPanel
+        expanded={expanded === "panel2"}
+        onChange={handleChange("panel2")}
+      >
+        <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+          2018-19
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          <a href="/" className="link">
+            Report for year 2018-19
+          </a>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
     </div>
   );
 }
